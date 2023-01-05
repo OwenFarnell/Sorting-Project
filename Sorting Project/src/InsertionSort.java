c49b9ca0b0d16202cda1a24e1793b12439ce9dfa
Owen Farnell <116113742+OwenFarnell@users.noreply.github.com>
2023-01-04 17:29:40 -0700

    Update InsertionSort.java

--- Sorting Project/src/InsertionSort.java
+++ Sorting Project/src/InsertionSort.java
@@ -1,50 +1,19 @@
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Random;
+public class InsertionSort
+	{
 
-public class InsertionSort {  
-	public static long start = System.currentTimeMillis();
-    public static void insertionSort(int array[]) {  
-        int n = array.length;  
-        for (int j = 1; j < n; j++) {  
-            int key = array[j];  
-            int i = j-1;  
-            while ( (i > -1) && ( array [i] > key ) ) {  
-                array [i+1] = array [i];  
-                i--;  
-            }  
-            array[i+1] = key;  
-        } 
-        long end = System.currentTimeMillis();
-        float sec = (end - start) / 15000F; System.out.println("- Insertion Sort took " + sec + " seconds");
-        
-       
-    }  
-       
-    public static void main(String a[]){    
-    	Scanner userStringInput = new Scanner(System.in);
-    	Scanner userInput = new Scanner(System.in);
+		public static void insertionSort(int[] elements)
+			{
+				for (int i = 1; i < elements.length; i++)
+					{
+						int temp = elements[i];
+						int possibleIndex = i;
+						while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+							{
+								elements[possibleIndex] = elements[possibleIndex - 1];
+								possibleIndex--;
+							}
+						elements[possibleIndex] = temp;
+					}
+			}
 
-    	System.out.println("How many #'s?");
-    	
-    	int ArrayLength = userInput.nextInt();
-    	int arr []= new int [ArrayLength];
-    	
-                 
-                for(int i =0; i < ArrayLength; i++) {
-            		int number = (int)(Math.random()*90)+10;
-            		arr[i] = number;  
-            	}
-           
-                  
-                insertionSort(arr);  
-                 
-                 
-                for(int i =0; i < ArrayLength; i++) {
-            		int number = (int)(Math.random()*90)+10;
-            		arr[i] = number;  
-            	}      
-    }    
-}  
+	}
