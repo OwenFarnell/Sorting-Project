a9dee26646e6d7df8bac909a91fbe9825a23810c
Owen Farnell <116113742+OwenFarnell@users.noreply.github.com>
2023-01-04 17:27:04 -0700

    Update MergeSort.java

--- Sorting Project/src/MergeSort.java
+++ Sorting Project/src/MergeSort.java
@@ -1,63 +1,73 @@
-import java.util.Scanner;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Random;
+public class MergeSort
+	{
+		public static void merge(int[] left_arr, int[] right_arr, int[] arr, int left_size, int j)
+			{
 
-public class MergeSort {  
-	
-	public static long start = System.currentTimeMillis();
-    static void mergeSort(int[] arr) {  
-        int n = arr.length;  
-        int temp = 0;  
-         for(int i=0; i < n; i++){ 
-        	 
-                 for(int j=1; j < (n-i); j++){  
-                	 
-                          if(arr[j-1] > arr[j]){
-                        	  
-                        temp = arr[j-1]; 
-                                 
-                                 
-                              arr[j-1] = arr[j];  
-                              arr[j] = temp;  
-                         }  
-                          
-                 }  
-         }
-         long end = System.currentTimeMillis();
-         float sec = (end - start) / 10000F; System.out.println("- Merge Sort took " + sec + " seconds");
-         
-         
-    }  
-    public static void main(String[] args) {  
-    	
-    	    
-    	Scanner userStringInput = new Scanner(System.in);
-    	Scanner userInput = new Scanner(System.in);
+				int i = 0, l = 0, r = 0;
+				// The while loops check the conditions for merging
+				while (l < left_size && r < j)
+					{
 
-    	System.out.println("How many #'s?");
-    	
-    	int ArrayLength = userInput.nextInt();
-    	int arr []= new int [ArrayLength];
-    	
-                 
-                for(int i =0; i < ArrayLength; i++) {
-            		int number = (int)(Math.random()*90)+10;
-            		arr[i] = number;  
-            	}
-                   
-                  
-                  
-                mergeSort(arr);  
-                 
-                 
-                for(int i =0; i < ArrayLength; i++) {
-            		int number = (int)(Math.random()*90)+10;
-            		arr[i] = number;  
-            	}     
-                
-        } 
-    
-}
+						if (left_arr[l] < right_arr[r])
+							{
+								arr[i++] = left_arr[l++];
+							} 
+						else
+							{
+								arr[i++] = right_arr[r++];
+							}
+					}
+				while (l < left_size)
+					{
+						arr[i++] = left_arr[l++];
+					}
+				while (r < j)
+					{
+						arr[i++] = right_arr[r++];
+					}
+			}
+
+		public static void mergeSort(int[] arr, int len)
+			{
+				if (len < 2)
+					{
+						return;
+					}
+
+				int mid = len / 2;
+				int[] left_arr = new int[mid];
+				int[] right_arr = new int[len - mid];
+
+				// Dividing array into two and copying into two separate arrays
+				int k = 0;
+				for (int i = 0; i < len; ++i)
+					{
+						if (i < mid)
+							{
+								left_arr[i] = arr[i];
+							} 
+						else
+							{
+								right_arr[k] = arr[i];
+								k = k + 1;
+							}
+					}
+				// Recursively calling the function to divide the subarrays further
+				mergeSort(left_arr, mid);
+				mergeSort(right_arr, len - mid);
+				// Calling the merge method on each subdivision
+				merge(left_arr, right_arr, arr, mid, len - mid);
+			}
+
+		public static void main(String args[])
+			{
+				int[] array =
+					{ 12, 1, 10, 50, 5, 15, 45 };
+				mergeSort(array, array.length);
+				for (int i = 0; i < array.length; ++i)
+					{
+						System.out.print(array[i] + " ");
+					}
+			}
+	}
 
