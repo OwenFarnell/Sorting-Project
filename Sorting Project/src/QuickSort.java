44b25ef2031ced7030f4b380be18d9d19980e538
Owen Farnell <116113742+OwenFarnell@users.noreply.github.com>
2023-01-04 17:26:25 -0700

    Update QuickSort.java

--- Sorting Project/src/QuickSort.java
+++ Sorting Project/src/QuickSort.java
@@ -1,58 +1,41 @@
-import java.util.Scanner;
+public class QuickSort
+	{
+		static int[] qsort(int items[])
+			{
+				qs(items, 0, items.length - 1);
+				return items;
+			}
 
-public class QuickSort {  
-	public static long start = System.currentTimeMillis();
-    public static void selectionSort(int[] arr)
-    	{  
-        
-    		for (int i = 0; i < arr.length - 1; i++)  
-        {  
-            int index = i;  
-            for (int j = i + 1; j < arr.length; j++)
-            	{  
-                if (arr[j] < arr[index])
-                	{
-                	
-                    index = j;
-                    
-                }  
-            }  
-            int smallerNumber = arr[index];   
-          arr[index] = arr[i];
-            
-            arr[i] = smallerNumber;  
-            
-        }
-        long end = System.currentTimeMillis();
-        float sec = (end - start) / 50000F; System.out.println("- Quick Sort took " + sec + " seconds");
-        
-      
-    }
-    
-       
-    public static void main(String a[]){  
-    	Scanner userStringInput = new Scanner(System.in);
-    	Scanner userInput = new Scanner(System.in);
+		private static void qs(int items[], int left, int right)
+			{
+				int i, j;
+				int pivot, temp;
+				i = left;
+				j = right;
+				pivot = items[(left + right) / 2];
 
-    	System.out.println("How many #'s?");
-    	
-    	int ArrayLength = userInput.nextInt();
-    	int arr []= new int [ArrayLength];
-    	
-                
-                for(int i =0; i < ArrayLength; i++) {
-            		int number = (int)(Math.random()*90)+10;
-            		arr[i] = number;  
-            	}
-                   
-                  
-                  
-                selectionSort(arr);  
-                 
-                  
-                for(int i =0; i < ArrayLength; i++) {
-            		int number = (int)(Math.random()*90)+10;
-            		arr[i] = number;  
-            	}  
-    }  
-}   
+				do
+					{
+						while ((items[i] < pivot) && (i < right))
+							i++;
+
+						while ((pivot < items[j]) && (j > left))
+							j--;
+
+						if (i <= j)
+							{
+								temp = items[i];
+								items[i] = items[j];
+								items[j] = temp;
+								i++;
+								j--;
+							}
+					} while (i <= j);
+
+				if (left < j)
+					qs(items, left, j);
+
+				if (i < right)
+					qs(items, i, right);
+			}
+	}
